c10ed8f9fd1a72aa85c60dbbbbbfdaba8b1a70ba
fatuu_mb <ndeyefatoumbengue20@gmail.com>
2023-05-26 15:17:58 +0000

    modif

---
+++ projet_reservation/app_reservation/views.py
@@ -0,0 +1,114 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from .models import Utilisateur
+import mysql.connector as sql
+
+config = {
+    'user': 'admin_reservation',
+    'password': 'Reservation123@',
+    'host': 'localhost',
+    'database': 'bd_app',
+}
+# Établir une connexion à la base de données
+conn = sql.connect(**config)
+cursor = conn.cursor()
+nom,prenom,email,telephone,adresse,mot_de_passe='','','','','',''
+# Create your views here.
+def index(request):
+    # Definir les variables globale
+    return render(request, 'index.html',  {})
+    
+    
+
+def connexion(request):
+    if request.method=="POST":
+        message=''
+        email = request.POST.get('email')
+        mot_de_passe = request.POST.get('mot_de_passe')
+        if(email!='' and mot_de_passe!=''): 
+            try:
+                message=''
+                conn = sql.connect(**config)
+                cursor = conn.cursor()
+                requete="select * from app_reservation_utilisateur where  email='{}' and  BINARY  mot_de_passe='{}'".format(email,mot_de_passe)
+                cursor.execute(requete)
+                res=cursor.fetchall()
+                if res==[]:
+                    message='Login ou mot de passe incorrect'
+                    cursor.close()
+                    conn.close()
+                    return render(request, 'connexion.html', {'erreur_message': message})
+                    
+                else :
+                    keys = ['id', 'nom', 'prenom', 'addresse', 'email', 'telephone']
+                    result = dict(zip(keys, res[0]))
+                    result['estConnecte'] = True
+                    cursor.close()
+                    conn.close()
+                    # Stocker le dictionnaire dans la session
+                    request.session['info_utilisateur'] = result
+                    print(request.session['info_utilisateur'].get('nom'))
+                    print('test')
+                    return redirect('index')
+                
+            except Exception as e:
+                print(str(e))  # Afficher l'erreur pour le débogage
+                message='Erreur route'
+                return render(request, 'connexion.html', {'erreur_message': message})
+        
+    # Effectuer des opérations
+    
+    # Renvoyer une réponse HTTP
+    #return HttpResponse('Un texte pour tester')
+    else:
+        context = {
+        'variable': 'Contenu dynamique'
+        }
+        return render(request, 'connexion.html', context)
+
+
+def inscription(request):
+    if request.method=="POST":
+        nom = request.POST.get('nom')
+        prenom = request.POST.get('prenom')
+        email = request.POST.get('email')
+        telephone = request.POST.get('telephone')
+        adresse = request.POST.get('adresse')
+        mot_de_passe = request.POST.get('mot_de_passe')
+        mot_de_passe2 =request.POST.get('mot_de_passe2')
+        if mot_de_passe!=mot_de_passe2:
+            return HttpResponse('Les mots de passe ne correspondent pas')
+        try:
+            requete="insert into app_reservation_utilisateur (id,nom,prenom,adresse,email,telephone,mot_de_passe) values (NULL,'{}','{}','{}','{}','{}','{}')".format(nom,prenom,adresse,email,telephone,mot_de_passe)
+            cursor.execute(requete)
+            cursor.close()
+            conn.close()
+            return redirect('connexion')  # Rediriger vers la page d'accueil après l'inscription réussie
+        except Exception as e:
+            print(str(e))  # Afficher l'erreur pour le débogage
+            return render(request, 'inscription.html', {'error_message': 'erreur'})
+        
+    # Effectuer des opérations
+    
+    # Renvoyer une réponse HTTP
+    #return HttpResponse('Un texte pour tester')
+    else:
+        context = {
+        'variable': 'Contenu dynamique'
+        }
+        return render(request, 'inscription.html', context)
+
+
+def resultat(request):
+    # Récupérer des données depuis le modèle
+    
+    context = {
+        'variable': 'Contenu dynamique'
+    }
+    # Effectuer des opérations
+    
+    # Renvoyer une réponse HTTP
+    #return HttpResponse('Un texte pour tester')
+    return render(request, 'resultat.html', context)
+
